24672f79b8dbd931b422448984df683847ff6a4f
bharathkumar2606 <108141298+bharathkumar2606@users.noreply.github.com>
2024-02-15 15:39:00 +0530

    Add files via upload

---
+++ GAMINGSITE/function.js
@@ -0,0 +1,9 @@
+
+var userName=document.getElementById("userName")
+var userPassword=document.getElementById("userPassword")
+function validate(){
+        if(userName=="bharath" && userPassword=="bharath@123")
+            alert("Login Successful")
+        else
+            alert("Incorrect Credentials")
+}
