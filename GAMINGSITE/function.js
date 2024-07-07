d5935500b84811f922df24da4b2a202387f840be
bharathkumar2606 <108141298+bharathkumar2606@users.noreply.github.com>
2024-07-07 19:28:42 +0530

    Delete GAMINGSITE directory

--- GAMINGSITE/function.js
+++
@@ -1,9 +0,0 @@
-
-var userName=document.getElementById("userName")
-var userPassword=document.getElementById("userPassword")
-function validate(){
-        if(userName=="bharath" && userPassword=="bharath@123")
-            alert("Login Successful")
-        else
-            alert("Incorrect Credentials")
-}
